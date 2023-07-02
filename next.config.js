5575e863925ce30debcaa4acf7b30878c11a58b1
Abha Meshram <abha25meshram@gmail.com>
2023-07-02 14:48:35 +0530

    add project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        domains: ['s3.ap-south-1.amazonaws.com'],
+      },
+}
